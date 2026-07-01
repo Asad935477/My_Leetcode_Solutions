d3cb62887b4e59733eaa628395b4243f91f14eea
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2026-07-01 18:23:50 +0000

    Add initial variable declarations in maximumSafenessFactor function

--- 2812/2812.Find the Safest Path in a Grid.js
+++ 2812/2812.Find the Safest Path in a Grid.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 var maximumSafenessFactor = function(grid) {
-    
+    const kIsThief = 1;
+    const kUnvisited = -1;
+    const rcTot = thiefGrid.length;
+    const kOffsets = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
 };
