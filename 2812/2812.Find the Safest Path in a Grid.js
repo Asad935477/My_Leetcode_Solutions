ad41e8523356d868acb32fcc1692b1a94042c65c
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2026-07-01 18:24:20 +0000

    Add early return condition in maximumSafenessFactor function

--- 2812/2812.Find the Safest Path in a Grid.js
+++ 2812/2812.Find the Safest Path in a Grid.js
@@ -8,4 +8,7 @@ var maximumSafenessFactor = function(grid) {
     const rcTot = thiefGrid.length;
     const kOffsets = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
+    if (thiefGrid[0][0] === kIsThief || thiefGrid[rcTot - 1][rcTot - 1] === kIsThief) {
+        return 0;
+    }
 };
