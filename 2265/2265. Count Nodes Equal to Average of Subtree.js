7184cf3140f75292d12a939d88c557fdb5f5f4c5
Asad Khan <asad.khan@modderstack.com>
2026-09-10 23:00:44 +0530

    Refactor indentation and formatting in averageOfSubtree function

--- 2265/2265. Count Nodes Equal to Average of Subtree.js
+++ 2265/2265. Count Nodes Equal to Average of Subtree.js
@@ -11,11 +11,10 @@
  * @param {TreeNode} root
  * @return {number}
  */
-
 var averageOfSubtree = function(root) {
     let answer = 0;
 
-     function dfs(node) {
+    function dfs(node) {
         if (node === null) {
             return [0, 0];
         }
@@ -25,5 +24,14 @@ var averageOfSubtree = function(root) {
 
         const sum = leftSum + rightSum + node.val;
         const count = leftCount + rightCount + 1;
+
+        if (node.val === Math.floor(sum / count)) {
+            answer++;
+        }
+
+        return [sum, count];
     }
+
+    dfs(root);
+    return answer;
 };
