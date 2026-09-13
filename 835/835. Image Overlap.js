e9401d6f36b7241f1969beae854339725d49a5d3
Asad Khan <asad.khan@modderstack.com>
2026-09-13 23:38:05 +0530

    Add complete implementation of largestOverlap function for problem 835: Image Overlap

--- 835/835. Image Overlap.js
+++ 835/835. Image Overlap.js
@@ -3,7 +3,31 @@
  * @param {number[][]} img2
  * @return {number}
  */
-
 var largestOverlap = function(img1, img2) {
-    
+    const n = img1.length;
+    let result = 0;
+
+    for (let dx = -n + 1; dx < n; dx++) {
+        for (let dy = -n + 1; dy < n; dy++) {
+            let score = 0;
+
+            for (let i = 0; i < n; i++) {
+                for (let j = 0; j < n; j++) {
+                    const newI = i + dx;
+                    const newJ = j + dy;
+
+                    if (
+                        0 <= newI && newI < n &&
+                        0 <= newJ && newJ < n
+                    ) {
+                        score += img1[newI][newJ] & img2[i][j];
+                    }
+                }
+            }
+
+            result = Math.max(result, score);
+        }
+    }
+
+    return result;
 };
