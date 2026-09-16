e34b30688fdedb3f802ebf9bd094f535e248554f
Asad Khan <asad.khan@modderstack.com>
2026-09-16 23:49:06 +0530

    Implement dynamic programming logic for problem 1621: Number of Sets of K Non-Overlapping Line Segments

--- 1621/1621. Number of Sets of K Non-Overlapping Line Segments.js
+++ 1621/1621. Number of Sets of K Non-Overlapping Line Segments.js
@@ -10,4 +10,18 @@ var numberOfSets = function(n, k) {
         { length: n },
         () => new Array(k + 1).fill(0)
     );
+
+     for (let i = 0; i < n; i++) {
+        dp[i][0] = 1;
+    }
+
+    for (let j = 1; j <= k; j++) {
+        let sum = 0;
+
+        for (let i = 1; i < n; i++) {
+            sum = (sum + dp[i - 1][j - 1]) % MOD;
+            dp[i][j] = (dp[i - 1][j] + sum) % MOD;
+        }
+    }
+    return dp[n - 1][k];
 }
