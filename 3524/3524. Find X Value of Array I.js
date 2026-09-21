264fc95bbce2f3d2fc34f5a2d438a94cf28ed609
Asad Khan <asad.khan@modderstack.com>
2026-09-21 23:48:17 +0530

    Implement resultArray function for problem 3524: Find X Value of Array I

--- 3524/3524. Find X Value of Array I.js
+++ 3524/3524. Find X Value of Array I.js
@@ -5,5 +5,19 @@
  */
  
 var resultArray = function(nums, k) {
-    
+    const result = new Array(k).fill(0);
+    let state = new Array(k).fill(0);
+    for (const value of nums) {
+        const rem = value % k;
+        const nextState = new Array(k).fill(0);
+        for (let r = 0; r < k; r++) {
+            const newRem = (r * rem) % k;
+            nextState[newRem] += state[r];
+            result[newRem] += state[r];
+        }
+        nextState[rem]++;
+        result[rem]++;
+        state = nextState;
+    }
+    return result;
 };
