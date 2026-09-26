a5c5db5295fd02d11b94dc8ac39f781d8af6836f
Asad Khan <asad.khan@modderstack.com>
2026-09-26 23:17:55 +0530

    Refactor evaluate function for problem 1807: Consolidate logic and improve readability

--- 1807/1807. Evaluate the Bracket Pairs of a String.js
+++ 1807/1807. Evaluate the Bracket Pairs of a String.js
@@ -4,5 +4,78 @@
  * @return {string}
  */
 var evaluate = function(s, knowledge) {
-    
+    let map = new Map();
+
+    for(let list of knowledge) {
+        map.set(list[0], list[1]);
+    }
+
+    let key = "";
+    let res = "";
+    let flag = false;
+
+    for(let ch of s) {
+        if(ch === '(') {
+            flag = true;
+        }
+        else if(ch === ')') {
+            if(map.has(key)) {
+                res += map.get(key);
+            }
+            else {
+                res += "?";
+            }
+
+            flag = false;
+            key = "";
+        }
+        else if(flag) {
+            key += ch;
+        }
+        else {
+            res += ch;
+        }
+    }
+
+    return res;
+};/**
+ * @param {string} s
+ * @param {string[][]} knowledge
+ * @return {string}
+ */
+var evaluate = function(s, knowledge) {
+    let map = new Map();
+
+    for(let list of knowledge) {
+        map.set(list[0], list[1]);
+    }
+
+    let key = "";
+    let res = "";
+    let flag = false;
+
+    for(let ch of s) {
+        if(ch === '(') {
+            flag = true;
+        }
+        else if(ch === ')') {
+            if(map.has(key)) {
+                res += map.get(key);
+            }
+            else {
+                res += "?";
+            }
+
+            flag = false;
+            key = "";
+        }
+        else if(flag) {
+            key += ch;
+        }
+        else {
+            res += ch;
+        }
+    }
+
+    return res;
 };
